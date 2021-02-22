5068fa57183931401419cb54afb3a246611689e0
Kvel4 <d.monakhov0@gmail.com>
2021-02-22 16:35:17 +0300

    fix

---
+++ java-solutions/info/kgeorgiy/ja/monakhov/arrayset/ArraySet.java
@@ -0,0 +1,91 @@
+package info.kgeorgiy.ja.monakhov.arrayset;
+
+import java.util.*;
+import java.util.SortedSet;
+
+public class ArraySet<E> extends AbstractSet<E> implements SortedSet<E> {
+    private List<E> list;
+    private Comparator<? super E> comparator;
+
+    public ArraySet(){
+        list = new ArrayList<>();
+    }
+
+    public ArraySet(Collection<? extends E> collection) {
+        list = new ArrayList<>(new TreeSet<>(collection));
+    }
+
+    public ArraySet(Collection<? extends E> collection, Comparator<? super E> comparator) {
+        TreeSet<E> treeSet = new TreeSet<>(comparator);
+        treeSet.addAll(collection);
+        this.comparator = comparator;
+        list = new ArrayList<>(treeSet);
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return Collections.unmodifiableList(list).iterator();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public Comparator<? super E> comparator() {
+        return comparator;
+    }
+
+    @Override
+    public ArraySet<E> subSet(E fromElement, E toElement) {
+        if (comparator.compare(fromElement, toElement) > 0) {
+            throw new IllegalArgumentException("From index must be lesser or equal than to index");
+        }
+        return subSet(find(fromElement), find(toElement));
+    }
+
+    @Override
+    public ArraySet<E> headSet(E toElement) {
+        return subSet(0, find(toElement));
+    }
+
+    @Override
+    public ArraySet<E> tailSet(E fromElement) {
+        return subSet(find(fromElement), list.size());
+    }
+
+    @Override
+    public E first() {
+        checkNonEmpty();
+        return list.get(0);
+    }
+
+    @Override
+    public E last() {
+        checkNonEmpty();
+        return list.get(list.size() - 1);
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return binarySearch((E) o) >= 0;
+    }
+
+    private void checkNonEmpty() {
+        if (list.size() == 0) throw new NoSuchElementException("Set must be non empty");
+    }
+
+    private int find(E element) {
+        int i = binarySearch(element);
+        return i >= 0 ? i : -i - 1;
+    }
+
+    private int binarySearch(E element) {
+        return Collections.binarySearch(list, Objects.requireNonNull(element), comparator);
+    }
+
+    private ArraySet<E> subSet(int from, int to) {
+        return new ArraySet<E>(list.subList(from, to), comparator);
+    }
+}
