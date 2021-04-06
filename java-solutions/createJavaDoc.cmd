95a1528d328d9648cb79a82f299148c31061d628
Kvel4 <d.monakhov0@gmail.com>
2021-04-06 20:19:41 +0300

    5-6

--- java-solutions/createJavaDoc.cmd
+++ java-solutions/createJavaDoc.cmd
@@ -1,6 +1,6 @@
 SET package=info.kgeorgiy.ja.monakhov.implementor
-SET implmod=..\..\java-advanced-2021\modules\info.kgeorgiy.ja.monakhov.implementor\
-SET dep=..\..\java-advanced-2021\modules\info.kgeorgiy.java.advanced.implementor\info\kgeorgiy\java\advanced\implementor\
+SET impl=modules\info.kgeorgiy.ja.monakhov.implementor\info\kgeorgiy\ja\monakhov\implementor\
+SET dep=modules\info.kgeorgiy.java.advanced.implementor\info\kgeorgiy\java\advanced\implementor\
 SET link=https://docs.oracle.com/en/java/javase/11/docs/api/
 
-javadoc -d javaDoc -link %link% -private -cp %implmod% %package% %dep%Impler.java %dep%JarImpler.java %dep%ImplerException.java
+javadoc -d javaDoc -link %link% -private %impl%Implementor.java %dep%Impler.java %dep%JarImpler.java %dep%ImplerException.java
