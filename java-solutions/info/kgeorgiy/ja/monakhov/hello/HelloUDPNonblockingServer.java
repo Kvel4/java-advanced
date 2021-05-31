0114f809ab19bfcf8182f6d749d595558d258a65
Kvel4 <d.monakhov0@gmail.com>
2021-05-31 14:49:05 +0300

    12 added

--- java-solutions/info/kgeorgiy/ja/monakhov/hello/HelloUDPNonblockingServer.java
+++ java-solutions/info/kgeorgiy/ja/monakhov/hello/HelloUDPNonblockingServer.java
@@ -1,185 +1,219 @@
 package info.kgeorgiy.ja.monakhov.hello;
 
-import info.kgeorgiy.java.advanced.hello.HelloClient;
+import info.kgeorgiy.java.advanced.hello.HelloServer;
 
 import java.io.IOException;
 import java.io.UncheckedIOException;
 import java.net.InetSocketAddress;
 import java.net.SocketAddress;
 import java.net.SocketException;
+import java.net.SocketOptions;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
 import java.nio.channels.SelectionKey;
 import java.nio.channels.Selector;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Objects;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.IntStream;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class HelloUDPNonblockingClient implements HelloClient {
+public class HelloUDPNonblockingServer implements HelloServer {
+    private final List<PortHandler> activeHandlers = new ArrayList<>();
+
     public static void main(final String[] args) throws IllegalArgumentException {
-        if (args == null || args.length != 5 || Arrays.stream(args).anyMatch(Objects::isNull)) {
-            System.err.println("You must pass 5 arguments: host port prefix threads requests");
-            return;
+        if (args == null || args.length != 2 || Arrays.stream(args).anyMatch(Objects::isNull)) {
+            throw new IllegalArgumentException("You must pass 2 arguments: port threads");
         }
-        final int port = Integer.parseInt(args[1]);
-        if (port < 0 || port > 65535) {
-            System.err.println("Port must be between 0 and 65535");
-            return;
-        }
-        new HelloUDPClient().run(args[0], port, args[2], Integer.parseInt(args[3]), Integer.parseInt(args[4]));
+        new HelloUDPNonblockingServer().start(Integer.parseInt(args[0]), Integer.parseInt(args[1]));
     }
 
     @Override
-    public void run(final String host, final int port, final String prefix, final int threads, final int requests) {
+    public void start(final int port, final int threads) {
         try {
-            new Sender(host, port, prefix, threads, requests).run();
+            activeHandlers.add(new PortHandler(port, threads));
+        } catch (final SocketException e) {
+            System.err.println("Unable to start socket on provided port: " + e.getMessage());
         } catch (final IOException e) {
-            System.err.println("Unable to open selector: " + e.getMessage());
+            System.err.println("Unable to open selector");
         }
     }
 
-    private static class Sender {
-        private final Selector selector;
+    @Override
+    public void close() {
+        activeHandlers.forEach(PortHandler::close);
+    }
+
+    private static class PortHandler implements AutoCloseable {
+        private final static int BUFFER_SIZE = Math.max(SocketOptions.SO_SNDBUF, SocketOptions.SO_RCVBUF);
+        private final static long SELECT_TIMEOUT_MILLISECONDS = 100;
+        private final int QUEUE_CAPACITY;
+
+        private final ExecutorService listener;
+        private final ExecutorService executor;
 
         private final SocketAddress address;
 
-        private final String prefix;
-        private final int threads;
-        private final int requests;
+        private final Selector selector;
 
-        public Sender(final String host, final int port, final String prefix, final int threads, final int requests) throws IOException {
+        private final BufferManager bufferManager;
+
+        public PortHandler(final int port, final int threads) throws IOException {
+            QUEUE_CAPACITY = threads;
+
+            listener = Executors.newSingleThreadExecutor();
+            executor = Executors.newFixedThreadPool(threads);
+
+            address = new InetSocketAddress(port);
             selector = Selector.open();
-            address = new InetSocketAddress(host, port);
+            bufferManager = new BufferManager();
 
-            this.prefix = prefix;
-            this.threads = threads;
-            this.requests = requests;
+            listener.submit(this::listen);
         }
 
-        public void run() {
-            try {
-                IntStream.range(0, threads).forEach(i -> {
-                    try {
-                        final DatagramChannel channel = HelloUtils.newDatagramChannel();
-                        channel.connect(address);
-                        channel.register(selector, SelectionKey.OP_WRITE, new Context(channel, prefix, i));
-                    } catch (final IOException e) {
-                        throw new UncheckedIOException(e);
-                    }
-                });
-            } catch (final UncheckedIOException e) {
-                System.err.println("Unable to open and configure datagram channels: " + e.getMessage());
-                return;
-            }
+        private void listen() {
+            try (final DatagramChannel channel = HelloUtils.newDatagramChannel()) {
+                channel.configureBlocking(false);
+                channel.bind(address);
+                channel.register(selector, SelectionKey.OP_READ);
 
-            while (!Thread.interrupted() && !selector.keys().isEmpty()) {
-                final AtomicBoolean isEmpty = new AtomicBoolean(true);
-                try {
+                while (channel.isOpen() && !Thread.interrupted()) {
                     selector.select(key -> {
-                        isEmpty.set(false);
                         try {
-                            final Context context = (Context) key.attachment();
                             if (key.isReadable()) {
-                                read(key, context);
-                                if (context.getRequestNumber() == requests) {
-                                    key.channel().close();
-                                    return;
-                                }
-                                key.interestOps(SelectionKey.OP_WRITE);
+                                final ByteBuffer buffer = bufferManager.getReceiveBuffer();
+                                final SocketAddress address = channel.receive(buffer);
+
+                                executor.submit(() -> process(key, buffer, address));
                             }
                             if (key.isWritable()) {
-                                write(key, context);
-                                key.interestOps(SelectionKey.OP_READ);
+                                final ResponseBuffer response = bufferManager.getResponseBuffer();
+                                final ByteBuffer buffer = response.getBuffer();
+
+                                channel.send(buffer, response.getAddress());
+                                bufferManager.addReceiveBuffer(buffer);
+
+                                key.interestOpsOr(SelectionKey.OP_READ);
+                            }
+                            if (bufferManager.isReceiveBufferEmpty()) {
+                                key.interestOpsAnd(~SelectionKey.OP_READ);
+                            }
+                            synchronized (key) {
+                                if (bufferManager.isResponseBufferEmpty()) {
+                                    key.interestOpsAnd(~SelectionKey.OP_WRITE);
+                                }
                             }
                         } catch (final IOException e) {
                             throw new UncheckedIOException(e);
                         }
-                    }, HelloUtils.SELECT_TIMEOUT_MILLISECONDS);
-                } catch (final IOException e) {
-                    System.err.println("IO exception occurred while selection: " + e.getMessage());
-                    return;
-                } catch (final UncheckedIOException e) {
-                    System.err.println("IO exception occurred while read/write to the channel: " + e.getMessage());
-                    return;
+                    }, SELECT_TIMEOUT_MILLISECONDS);
                 }
-
-                if (isEmpty.get()) {
-                    selector.keys().forEach(key -> {
-                        if (isReadInterest(key)) {
-                            try {
-                                write(key, (Context) key.attachment());
-                            } catch (final IOException ignored) {
-                            }
-                        }
-                    });
-                }
+            } catch (final IOException e) {
+                System.err.println("Unable to open datagram channel:" + e.getMessage());
+            } catch (final UncheckedIOException e) {
+                System.err.println("Some errors occurred while reading/writing to datagram channel: " + e.getMessage());
             }
         }
 
-        private void read(final SelectionKey key, final Context context) throws IOException {
-            final ByteBuffer buffer = context.getClearBuffer();
-            final DatagramChannel channel = (DatagramChannel) key.channel();
-
-            channel.read(buffer);
+        private void process(final SelectionKey key, final ByteBuffer buffer, final SocketAddress address) {
             buffer.flip();
-            final String request = context.getRequestBody();
-            final String response = HelloUtils.newString(buffer);
-
-            if (isValid(request, response)) {
-                System.out.println(response);
-                context.incrementRequestNumber();
+            bufferManager.addResponseBuffer(buffer, address);
+            synchronized (key) {
+                key.interestOpsOr(SelectionKey.OP_WRITE);
             }
         }
 
-        private void write(final SelectionKey key, final Context context) throws IOException {
-            final ByteBuffer buffer = context.getClearBuffer();
-            final DatagramChannel channel = (DatagramChannel) key.channel();
-
-            buffer.put(HelloUtils.getBytes(context.getRequestBody())).flip();
-            channel.write(buffer);
-            buffer.rewind();
+        @Override
+        public void close() {
+            try {
+                selector.close();
+            } catch (final IOException e) {
+                System.err.println("Unable to close selector" + e.getMessage());
+            }
+            listener.shutdownNow();
+            try {
+                if (!listener.awaitTermination(1, TimeUnit.SECONDS)) {
+                    System.err.println("Port listener isn't shutdown");
+                }
+            } catch (final InterruptedException ignored) {
+            }
+            executor.shutdownNow();
+            try {
+                if (!executor.awaitTermination(1, TimeUnit.SECONDS)) {
+                    System.err.println("Executor isn't shutdown");
+                }
+            } catch (final InterruptedException ignored) {
+            }
         }
 
-        private static boolean isValid(final String requestBody, final String responseBody) {
-            return responseBody.contains(requestBody);
+        private class BufferManager {
+            private final ArrayBlockingQueue<ResponseBuffer> responseBuffers = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
+            private final ArrayBlockingQueue<ByteBuffer> receiveBuffers = new ArrayBlockingQueue<>(QUEUE_CAPACITY);
+
+            public BufferManager() {
+                for (int i = 0; i < QUEUE_CAPACITY; i++) {
+                    addReceiveBuffer(ByteBuffer.allocate(BUFFER_SIZE).put(HelloUtils.getBytes("Hello, ")));
+                }
+            }
+
+            public void addReceiveBuffer(final ByteBuffer buffer) {
+                try {
+                    receiveBuffers.add(buffer.clear().put(HelloUtils.getBytes("Hello, ")));
+                } catch (final IllegalStateException ignored) {
+                }
+            }
+
+            public void addResponseBuffer(final ByteBuffer buffer, final SocketAddress address) {
+                try {
+                    responseBuffers.add(new ResponseBuffer(buffer, address));
+                } catch (final IllegalStateException ignored) {
+                }
+            }
+
+            public ByteBuffer getReceiveBuffer() {
+                try {
+                    return receiveBuffers.poll(1, TimeUnit.NANOSECONDS);
+                } catch (final InterruptedException ignored) { }
+                return null;
+            }
+
+            public ResponseBuffer getResponseBuffer() {
+                try {
+                    return responseBuffers.poll(1, TimeUnit.NANOSECONDS);
+                } catch (final InterruptedException ignored) { }
+                return null;
+            }
+
+            public boolean isReceiveBufferEmpty() {
+                return receiveBuffers.size() == 0;
+            }
+
+            public boolean isResponseBufferEmpty() {
+                return responseBuffers.size() == 0;
+            }
         }
 
-        private static boolean isReadInterest(final SelectionKey key) {
-            return (key.interestOps() & SelectionKey.OP_READ) == SelectionKey.OP_READ;
-        }
-
-        private static class Context {
+        private static class ResponseBuffer {
             private final ByteBuffer buffer;
-            private final String prefix;
-            private final int thread;
-            private int requestNumber;
+            private final SocketAddress address;
 
-            private Context(final DatagramChannel channel, final String prefix, final int thread) throws SocketException {
-                this.prefix = prefix;
-                this.thread = thread;
-                this.requestNumber = 0;
-                buffer = ByteBuffer.allocate(HelloUtils.getBufferSize(channel.socket()));
+            public ResponseBuffer(final ByteBuffer buffer, final SocketAddress address) {
+                this.buffer = buffer;
+                this.address = address;
             }
 
             public ByteBuffer getBuffer() {
                 return buffer;
             }
 
-            public ByteBuffer getClearBuffer() {
-                return buffer.clear();
-            }
-
-            public String getRequestBody() {
-                return prefix + thread + "_" + requestNumber;
-            }
-
-            public int getRequestNumber() {
-                return requestNumber;
-            }
-
-            public void incrementRequestNumber() {
-                requestNumber++;
+            public SocketAddress getAddress() {
+                return address;
             }
         }
     }
